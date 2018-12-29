46d0791ade7e49eb82fff2ef3df820de2cea504c
Nampo1224 <y.nanpo@gmail.com>
2018-12-29 14:24:12 +0900

    Run()とDraw()をどういったタイミングで行うか検討必要

--- Nampo_STG/Nampo_STG/Form1.Designer.cs
+++ Nampo_STG/Nampo_STG/Form1.Designer.cs
@@ -62,7 +62,7 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 3;
+            this.timer1.Interval = 17;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
